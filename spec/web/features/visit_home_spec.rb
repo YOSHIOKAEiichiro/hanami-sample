8df8bcec5dc72a6be740766b092db0b3c93dcdc8
YOSHIOKA Eiichiro <yoshioka-e@kamakura-net.co.jp>
2019-01-16 15:56:21 +0900

    初期表示完了

---
+++ spec/web/features/visit_home_spec.rb
@@ -0,0 +1,9 @@
+# spec/web/features/visit_home_spec.rb
+require 'features_helper'
+
+RSpec.describe 'Visit home' do
+  it 'is successful' do
+    visit '/'
+    expect(page).to have_content('Bookshelf')
+  end
+end
